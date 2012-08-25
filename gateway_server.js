866c295b55593d12e55150625351fec68c5e61f4
Michael Anderson <michael.anderson@runwithrobots.com>
2012-08-26 06:57:46 +0800

    Refactor and made login kinda work

--- gateway_server.js
+++ gateway_server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var url = require('url');
+var request = require('request');
 var querystring = require('querystring');
 var channels = require('./components/channels');
 
@@ -25,6 +26,10 @@ msg_queue.get_messages = function( user_id )
   return retval;
 }
 
+//TODO: Make these nicer
+player_registry = {}
+player_registry_rev = {}
+
 channels.subscribe('AUTH', function(channel,data)
 {
   var value = JSON.parse( data.toString() );
@@ -32,115 +37,169 @@ channels.subscribe('AUTH', function(channel,data)
   msg_queue.add_message( value.id, value );
 } )
 
+function getArgs()
+{
+  var args = null;
+  if ( "args" in this.post_data )
+  {
+    args = this.post_data.args;
+  }
+  else if ( "args" in this.url_info.query )
+  {
+     args = this.url_info.query.args;
+  }
+  else
+  {
+    throw new Error("No args argument provided");
+  }
 
-function onRequest( request, response ) {
+  try
+  {
+    //NOTE: This should accept both url safe base64 and traditional base64.
+    //NOTE: Node doesn't seem to care if you have the "==" padding to 4
+    //      bytes at the end either.  So we dont need to manually pad out the
+    //      correct number of bytes.
+    args.replace(/-/g,'+').replace(/_/g,'/');
+    return JSON.parse(Buffer( args, "base64").toString("utf8"));
+  }
+  catch(e)
+  {
+    throw new Error("Invalid args argument");
+  }
+}
 
-  var data_chunks = [];
+function onAuth( response )
+{
 
-  request.addListener("data", function(chunk) {
-    data_chunks.push( chunk )
-  } );
+  if( ! ("name" in this.args ) )
+  { 
+    //TODO: Check this error code
+    this.response.writeHead(300, {"Content-Type":"application/json"} );
+    this.response.write( JSON.stringify( {"error":"Expected name [string] in args"} ) );
+    this.response.end();
+    return;
+  }
 
-  request.addListener("end", function(chunk) {
+  if( ! ("password" in this.args ) )
+  { 
+    //TODO: Check this error code
+    this.response.writeHead(300, {"Content-Type":"application/json"} );
+    this.response.write( JSON.stringify( {"error":"Expected password [string] in args"} ) );
+    this.response.end();
+    return;
+  }
+
+  // Get the user with the given name
+  var name = this.args.name;
+  var password = this.args.password;
+  var this_ = this;
+
+  request(
+      'https://drmikeando.iriscouch.com/roardemo/_design/users/_view/name?' +
+          querystring.stringify( {"key":JSON.stringify( name ) } ),
+          function( error, rr, body) { onDBResult.call(this_, error, rr, body, password); }
+      );
+}
+
+//TODO: Need to handle case where error is passed
+function onDBResult( error, rr, body, password )
+{
+  var body_json = JSON.parse(body);
+  console.log(body_json);
+  if(!body_json)
+   {
+    //TODO: Check this error code
+    this.response.writeHead(300, {"Content-Type":"application/json"} );
+    this.response.write( JSON.stringify( {"error":"CouchDB JSON Error"} ) );
+    this.response.end();
+    return;
+   }
+  if(
+      body_json.rows.length == 0 ||
+      body_json.rows[0].value.password !== password
+    )
+  {
+    //TODO: Check this error code
+    this.response.writeHead(300, {"Content-Type":"application/json"} );
+    this.response.write( JSON.stringify( {"error":"Authorization failed"} ) );
+    this.response.end();
+    console.log( body_json.rows[0].value.password , "=/=", password );
+    return;
+  }
+
+  this.response.writeHead(200, {"Content-Type":"application/json"});
+  var user_id = body_json.rows[0].value.user_id;
+
+  var this_ = this;
+  require('crypto').randomBytes(48, function(ex, buf)
+  {
+    var auth_token = buf.toString('base64').replace(/\//g,'_').replace(/\+/g,'-');
+    var result = { "id":user_id, "auth_token":auth_token }
+    result.messages = msg_queue.get_messages( user_id );
+    this_.response.write( JSON.stringify( result  ) );
+    this_.response.end()
+
+    channels.send( "AUTH", JSON.stringify({ "id":user_id, "mesg":"I is teh roxxor" } ) );
+    if ( user_id in player_registry_rev ) { delete player_registry[ player_registry_rev[user_id] ]; }
+    player_registry[auth_token] = user_id;
+    player_registry_rev[user_id] = auth_token;
+
+    console.log( player_registry );
+    console.log( player_registry_rev );
+  });
+}
+
+function onPoll()
+{
+  // TODO: Check that we got an auth_token
+  console.log("Got poll request ... just faking it");
+  this.response.writeHead(200, {"Content-Type":"application/json"});
+  // TODO: Get real messages
+  this.response.write( JSON.stringify( { "messages":msg_queue.get_messages( "_some_opaque_user_id_" ) } ) );
+  this.response.end();
+}
+
+function onUnknown()
+{
+  console.log("Unknown request...");
+  this.response.writeHead(400,{"Content-Type":"application/json"});
+  this.response.write( JSON.stringify( {"error":"Invalid request url"} ) );
+  this.response.end();
+}
+
+function onRequestEnd( req, response, data_chunks )
+{
+    var req_obj = {}
+
+    req_obj.url_info = url.parse( req.url, true );
+    req_obj.post_data = {}
+
     var data = Buffer.concat(data_chunks);
-
-    var url_info = url.parse( request.url, true );
-    var post_data = {}
 
     if( data.length > 0 )
     {
-      post_data = querystring.parse( data.toString("ascii") );
+      req_obj.post_data = querystring.parse( data.toString("ascii") );
     }
 
-    if( url_info.pathname == "/auth" )
-    {
-      var args = null;
-      if ( "args" in post_data ) { args = post_data.args; }
-      else if ( "args" in url_info.query ) { args = url_info.query.args; }
-      else
-      {
-        //TODO: Check this error code
-        response.writeHead(400, {"Content-Type":"application/json"} );
-        response.write( JSON.stringify( {"error":"/auth expects base64 encoded args argument"} ) );
-        response.end();
-        return;
-      }
+    req_obj.args = getArgs.call(req_obj)
+    req_obj.response = response;
 
-      try {
-        //NOTE: This should accept both url safe base64 and traditional base64.
-        //NOTE: Node doesn't seem to care if you have the "==" padding to 4
-        //      bytes at the end either.  So we dont need to manually pad out the
-        //      correct number of bytes.
-        args.replace(/-/g,'+').replace(/_/g,'/');
-        args = JSON.parse(Buffer( args, "base64").toString("utf8"));
-      }
-      catch(e)
-      {
-         if( e instanceof(SyntaxError) )
-         {
-           //TODO: Check this error code
-           response.writeHead(300, {"Content-Type":"application/json"} );
-           response.write( JSON.stringify( {"error":"Invalid args argument"} ) );
-           response.end();
-           return;
-         }
-      }
+    if( req_obj.url_info.pathname == "/auth" ) { onAuth.call( req_obj ); }
+    else if( req_obj.url_info.pathname  == "/poll" )      { onPoll.call( req_obj ); }
+    else                               { onUnknown.call( req_obj ); }
+}
 
-      if( ! ("name" in args ) )
-      { 
-        //TODO: Check this error code
-        response.writeHead(300, {"Content-Type":"application/json"} );
-        response.write( JSON.stringify( {"error":"Expected name [string] in args"} ) );
-        response.end();
-        return;
-      }
+function onRequest( req, response )
+{
+  var data_chunks = [];
 
-      if( ! ("password" in args ) )
-      { 
-        //TODO: Check this error code
-        response.writeHead(300, {"Content-Type":"application/json"} );
-        response.write( JSON.stringify( {"error":"Expected password [string] in args"} ) );
-        response.end();
-        return;
-      }
-
-      // TODO: Do some real auth here.
-      if( args.name !== "monkey" || args.password !== "gorgonzola" )
-      {
-        //TODO: Check this error code
-        response.writeHead(300, {"Content-Type":"application/json"} );
-        response.write( JSON.stringify( {"error":"Authorization failed"} ) );
-        response.end();
-        return;
-      }
-
-      response.writeHead(200, {"Content-Type":"application/json"});
-      var user_id = "_some_opaque_user_id_";
-      var result = { "id":user_id, "auth_token":"_some_opaque_auth_token" }
-      result.messages = msg_queue.get_messages( user_id );
-      response.write( JSON.stringify( result  ) );
-
-      channels.send( "AUTH", JSON.stringify({ "id":user_id, "mesg":"I is teh roxxor" } ) )
-    }
-    else if( request.url == "/poll" )
-    {
-      // TODO: Check that we got an auth_token
-      console.log("Got poll request ... just faking it");
-      response.writeHead(200, {"Content-Type":"application/json"});
-      // TODO: Get real messages
-      response.write( JSON.stringify( { "messages":msg_queue.get_messages( "_some_opaque_user_id_" ) } ) );
-    }
-    else
-    {
-      console.log("Unknown request...");
-      response.writeHead(400,{"Content-Type":"application/json"});
-      response.write( JSON.stringify( {"error":"Invalid request url"} ) );
-    }
-
-
-    response.end();
+  req.addListener("data", function(chunk) {
+    data_chunks.push( chunk )
   } );
 
+  req.addListener("end", function() {
+    onRequestEnd( req, response, data_chunks );
+  } );
 }
 
 http.createServer(onRequest).listen(8083)
