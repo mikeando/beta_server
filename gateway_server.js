bcfcababb7cd86b6965ff4aa3ae43be94a85e3e5
Michael Anderson <michael.anderson@runwithrobots.com>
2012-08-26 00:40:08 +0800

    Pulled zmq stuff into channels object and made named channels work

--- gateway_server.js
+++ gateway_server.js
@@ -5,33 +5,80 @@ var http = require('http')
 // And we'll listen for messages that should be sent back to the user
 // Based on their username
 
-//TODO: Hide the ZMQ implementation inside an object to make later changes
-//      easier.  and to enable sharing of implementation between applications.
+var channels = {};
+channels._zmq = {};
+channels._zmq.sockets = {};
+channels._zmq.sockets.push =  zmq.socket("push");
+channels._zmq.sockets.sub  = zmq.socket("sub");
 
-var z_push = zmq.socket("push")
-var z_sub = zmq.socket("sub")
+channels._zmq.sockets.sub.connect("tcp://localhost:5577");
+channels._zmq.sockets.push.connect("tcp://localhost:5578");
 
-z_sub.connect("tcp://localhost:5577")
-z_push.connect("tcp://localhost:5578")
+channels.subscriptions = {};
 
-// For now take all messages - we'll filter them ourself.
-z_sub.subscribe('')
+channels.subscribe = function( channel_name, fn )
+{
+  //NOTE: Channel names are 4 ascii characters
+  if( channel_name.length !== 4 ) { throw new Error("Channel names must be 4 characters long"); }
+
+  if( !( channel_name in this.subscriptions ) )
+  {
+    this._zmq.sockets.sub.subscribe( channel_name );
+    this.subscriptions[channel_name] = [];
+  }
+
+  if( ! this.subscriptions.hasOwnProperty(channel_name) )
+  {
+    throw new Error("Invalid channel_name");
+  }
+
+  this.subscriptions[channel_name].push(fn);
+};
+
+// We've got a response
+channels._zmq.sockets.sub.on('message',
+    function(data)
+    {
+      var channel_name = data.toString("ascii",0,4);
+      var channel_data = data.slice(4);
+
+      console.log("NAME : "+channel_name );
+      console.log("DATA : "+channel_data.toString("utf-8") )
+
+      //TODO: Work out what to do is the subscription functions throw an error.
+      //      Note that storing the errors in an array and rethrowing once done
+      //      loses the stack trace making them very hard to locate.
+
+      if( channels.subscriptions.hasOwnProperty( channel_name ) )
+      {
+        for( var i =0 ; i<channels.subscriptions[channel_name].length; ++i )
+        {
+            channels.subscriptions[channel_name][i](channel_name,channel_data)
+        }
+      }
+    }
+);
+
+channels.send = function(channel_name,mesg)
+{
+  //NOTE: Channel names are 4 ascii characters
+  if( channel_name.length !== 4 ) { throw new Error("Channel names must be 4 characters long"); }
+  channels._zmq.sockets.push.send(channel_name+mesg)
+}
 
 // The list of pending messages to give to the user
 // TODO : Make this a real class
 var msg_queue = []
 
-// We've got a response
-z_sub.on('message',
-function(data)
+// For now take all messages - we'll filter them ourself.
+channels.subscribe('AUTH', function(channel,data)
 {
-  var value = JSON.parse( data.toString() )
-  console.log("Got me a "+data.toString() )
+  var value = JSON.parse( data.toString() );
+  console.log("Got me a "+data.toString() );
   // TODO: Check its a message that should go to a player.
   // TODO: Store it in the msg queue
-  msg_queue.push( value )
-})
-
+  msg_queue.push( value );
+} )
 
 
 function onRequest( request, response ) {
@@ -40,7 +87,7 @@ function onRequest( request, response ) {
     console.log("Got auth request ... just faking it");
     response.writeHead(200, {"Content-Type":"application/json"});
     response.write( JSON.stringify( { "id":"_some_opaque_user_id_", "auth_token":"_some_opaque_auth_token" } ) );
-    z_push.send( JSON.stringify( { "auth":"_some_opaque_user_id_" } ) )
+    channels.send( "AUTH", JSON.stringify({ "auth":"_some_opaque_user_id_" } ) )
     // TODO: Do some real auth here.
     // TODO: Check if you've got any messages queued up for the user
   }
